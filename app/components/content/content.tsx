750fbe373608bc46432b767b0579b4ff143f1598
Luã Rodrigues <961104@lojasrenner.com.br>
2024-02-07 09:33:00 -0300

    fix: box layout

--- app/components/content/content.tsx
+++ app/components/content/content.tsx
@@ -2,21 +2,28 @@ import React, { useState } from 'react';
 import { BoxContent } from '../box/box-content';
 import { Background } from '../background/background';
 import Head from "next/head";
+import styles from './Content.module.scss'; 
 
 export const Content: React.FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  }
 
   return (
     <Background>
       <Head>
         <title>Home</title>
       </Head>
-      <BoxContent isOpen={modalOpen} onClose={() => setModalOpen(false)} title={"Box title"} >
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      </BoxContent>
-      <button onClick={() => setModalOpen(true)}>abrir modal</button>
+      <div className={styles.centeredContent}> 
+        <BoxContent isOpen={modalOpen} onClose={() => setModalOpen(false)} title={"Box title"} >
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </BoxContent>
+        <button onClick={handleOpenModal} className={styles.button} >abrir modal</button>
+      </div>
     </Background>
   );
 };
